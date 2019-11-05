c71b82c0232dfea190470732f9ae2ab17d7dffec
Maksim Kalachou <Maksim_Kalachou@epam.com>
2019-11-05 16:02:57 +0300

    fix: join 2 types of items into 1 array

--- src/app/models/library.interface.ts
+++ src/app/models/library.interface.ts
@@ -2,6 +2,7 @@ import { IMovie } from './movie.interface';
 import { ITvShow } from './tvshow.interface';
 
 export interface ILibrary {
-    movies: Array<IMovie>;
-    shows: Array<ITvShow>;
+/*     movies: Array<IMovie>;
+    shows: Array<ITvShow>; */
+    items: (IMovie|ITvShow)[];
 }
