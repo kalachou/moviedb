beb0010838d4bbf0cba86f321a24f727c8b96846
Maksim Kalachou <mvkalacho>
2019-11-22 18:41:23 +0300

    feat: add movie servise implementation

--- src/app/servises/movies.servise.ts
+++ src/app/servises/movies.servise.ts
@@ -12,29 +12,39 @@ export class MoviesServise {
 
   private initialPage = 1;
 
+  constructor(private http: HttpClient) { }
+
   public getMoviesPage(page: number = this.initialPage) {
-    let arr;
-    const result = forkJoin(
+    let resultArray: Movie[];
+    let resultRequest: any;
+
+    forkJoin(
       this.http.get(`https://api.themoviedb.org/3/movie/popular?page=${page}&language=en-US&api_key=0de915948d52603403cc6102091989f6`)
-    );
-    arr = result.results;
-    //convert into camelcased
-    
-    //average vote
+    ).subscribe((data: any) => resultRequest = data.results);
+
+    // convert into camelcased and remove unnesessary proprties
+    resultArray = resultRequest.map((x: { vote_average: any; poster_path: any; overview: any; title: any; }) => ({
+      voteAverage: x.vote_average,
+      posterPath: `https://image.tmdb.org/t/p/w600_and_h900_bestv2${x.poster_path}`,
+      overview: x.overview,
+      title: x.title
+    }));
+
+    return resultArray;
+
+    /*
+    // average vote
     arr[0].vote_average
 
-    //picture
+    // picture
     `https://image.tmdb.org/t/p/w600_and_h900_bestv2${arr[0].poster_path}`
 
-    //info
+    // info
     arr[0].overview
 
-    //title
+    // title
     arr[0].title
-
-
-
-    return result: Movie[];
+    */
   }
 
 }
