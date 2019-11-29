ad0f949983e17e59f9508bf0f4eeaf2b72da821e
Maksim Kalachou <mvkalacho>
2019-11-29 19:30:48 +0300

    feat: add selectedMovie into MoviesPageState interface

--- src/app/store/state/moviesPage.state.ts
+++ src/app/store/state/moviesPage.state.ts
@@ -1,9 +1,10 @@
 import { RouterReducerState } from '@ngrx/router-store';
 
 import { Movie } from '../../models/movie.interface';
-import { MoviesPage } from 'src/app/models/moviesPage.interface';
+import { MoviesPage } from '../../models/moviesPage.interface';
 
 export interface MoviesPageState {
+    selectedMovie: Movie;
     router?: RouterReducerState;
     page: number;
     items: Movie[];
@@ -11,5 +12,6 @@ export interface MoviesPageState {
 
 export const initialMoviesPageState: MoviesPage = {
     page: 1,
-    items: new Array<Movie>(20)
+    items: new Array<Movie>(20),
+    selectedMovie: null
 };
