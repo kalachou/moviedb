ce6045a64b9c7b476a3737265d357d1125aa155b
Maksim Kalachou <mvkalacho>
2019-11-29 19:23:01 +0300

    feat: add loadNextPage method, initialise 1st page

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter, HostListener } from '@angular/core';
-import { Movie } from 'src/app/models/movie.interface';
+import { Movie } from '../../models/movie.interface';
+import { Store, select } from '@ngrx/store';
+import { AppState } from '../../store/state/app.state';
+import { LoadPage } from '../../store/actions/moviesPage.actions';
+import { selectCurrentPage } from '../../store/selectors/moviesPage.selectors';
 
 @Component({
   selector: 'app-movies',
@@ -7,8 +11,9 @@ import { Movie } from 'src/app/models/movie.interface';
   styleUrls: ['./movies.component.sass']
 })
 export class MoviesComponent implements OnInit {
+  private _nextPage;
 
-  constructor() { }
+  constructor(private _store: Store<AppState>) { }
   @Input()
   movies: Movie[];
   @Output()
@@ -17,15 +22,22 @@ export class MoviesComponent implements OnInit {
   @HostListener('window:scroll', [])
   onScroll(): void {
     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2) {
-      alert("you're at the bottom of the page");
+      // alert("you're at the bottom of the page");
+      this.loadNextPage();
     }
   }
 
   ngOnInit() {
+    this._store.dispatch(new LoadPage());
   }
 
   navigateToMovie(id: number) {
     this.movieSelected.emit(id);
+  }
+
+  loadNextPage() {
+    this._store.select(selectCurrentPage).subscribe(res => this._nextPage = res + 1);
+    this._store.dispatch(new LoadPage(this._nextPage));
   }
 
 
