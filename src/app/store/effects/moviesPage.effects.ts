be571ed0c684dcccd8a188b0d9c3c965cfe1df8d
Maksim Kalachou <mvkalacho>
2019-11-29 19:26:50 +0300

    fix: change returning type of getMoviesPage effect

--- src/app/store/effects/moviesPage.effects.ts
+++ src/app/store/effects/moviesPage.effects.ts
@@ -3,26 +3,30 @@ import { Store } from '@ngrx/store';
 import { switchMap } from 'rxjs/operators';
 import { MoviesService } from '../../services/movies.service';
 import { Injectable } from '@angular/core';
-import { EMoviesPageActions, LoadPage } from '../actions/moviesPage.actions';
+import { EMoviesPageActions, LoadPage, LoadPageSucces } from '../actions/moviesPage.actions';
 import { AppState } from '../state/app.state';
+import { of } from 'rxjs';
 
 @Injectable()
 export class MoviesPageEffects {
-    constructor(
-      private _actions$: Actions,
-      private _store: Store<AppState>,
-      private _moviesService: MoviesService
-    ) {
-
-}
-
-@Effect()
- getMoviesPage$ = this._actions$.pipe(
+  @Effect()
+  getMoviesPage$ = this._actions$.pipe(
     ofType<LoadPage>(EMoviesPageActions.LoadPage),
     switchMap((action: LoadPage) => {
       const pageNumber = action.payload;
-      return this._moviesService.getMoviesPage(pageNumber);
+      const moviesChunk =  this._moviesService.getMoviesPage(pageNumber);
+      return of(new LoadPageSucces(moviesChunk));
     })
-);
+  );
+
+  constructor(
+    private _actions$: Actions,
+    private _store: Store<AppState>,
+    private _moviesService: MoviesService
+  ) {
+  }
+
+
+
 
 }
